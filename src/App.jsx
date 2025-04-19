08d35208739022ec696d28a9b66247e617d387b7
Mukhammadali <108802022+mukhammadali010@users.noreply.github.com>
2025-04-19 10:55:13 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Shorustamav Hojiakbar</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa non,
+        blanditiis quidem aut impedit, quis cumque maiores quae ut veritatis
+        pariatur eligendi dolores sunt ipsam sequi suscipit unde asperiores
+        odit?
+      </p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>TAILWIND CSS</li>
+        <li>JAVASCRIPT</li>
+        <li>REACT</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
